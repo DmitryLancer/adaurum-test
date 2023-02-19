9373f90453a409c6612abdf132478b1ec499a64d
Dmitry <dmitrylancer.it@yandex.ru>
2023-02-19 18:14:23 +0300

    post link

--- src/PostMapper.php
+++ src/PostMapper.php
@@ -2,6 +2,7 @@
 
 namespace Adaurum;
 
+use Exception;
 use PDO;
 
 class PostMapper
@@ -22,5 +23,15 @@ class PostMapper
 
         $result = $statement->fetchAll();
         return array_shift($result);
+    }
+
+    public function getList(string $direction): ?array
+    {
+        if (!in_array($direction, ['DESC', 'ASC'])) {
+            throw new Exception('The direction is not supported.');
+        }
+        $statement = $this->connection->prepare('SELECT * FROM post ORDER BY published_date ' . $direction);
+        $statement->execute();
+        return $statement->fetchAll();
     }
 }
